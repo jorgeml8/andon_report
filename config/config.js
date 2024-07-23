2b3e71398fcad8aade0b3a1ee930ae6dbb16d9eb
jorgeml <40178446+jorgeml8@users.noreply.github.com>
2024-07-23 15:56:00 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: 3000,
-    MongoDBHost: 'mongodb://admin:admin@raqamssofprd02v.mcquay.com:27017/',
+    MongoDBHost: 'mongodb://user:password@raqamssofprd02v.mcquay.com:27017/',
     MongoDBName: 'andon_messages',
 
   };
